4390859b1120ec8c31b31333cbfee306bb4281af
Konstantin Ebeling <ebeling.konstantin@gmail.com>
2019-07-03 20:18:11 +0200

    add chooos #4

--- commands/fun/choose.js
+++ commands/fun/choose.js
@@ -1,6 +1,6 @@
 const commando = require('discord.js-commando');
 
-module.exports = class EchoCommand extends commando.Command {
+module.exports = class ChooseCommand extends commando.Command {
   constructor(client) {
     super(client, {
       name: 'choose',
@@ -20,9 +20,27 @@ module.exports = class EchoCommand extends commando.Command {
   async run(msg, {
     method
   }) {
-    const guild = client.guilds;
-    console.log(guild);
-    var user = 'test';
+    var user;
+    const commandoMembers = msg.member.guild.members; //Extract server data from message
+    var filteredMembers;
+
+    if (method == 'online') filteredMembers = commandoMembers.filter(m => m.presence.status === 'online'); // the super weird map but it's filtered first
+    else if (method == 'all') filteredMembers = commandoMembers; //just the super weird map
+    else {
+      msg.reply('Please use **online** or **all** to define from what pool you want to select a user from!');
+      return;
+    }
+
+    const membersArray = Array.from(filteredMembers); //Super weird map to 2d array
+
+    var members = []; //easy to work with users array
+    for (var i in membersArray) {
+      members.push(membersArray[i][1].user); //weird 2d array to simple array
+    }
+    console.log(members.length);
+    console.log(members);
+    user = members[Math.floor(Math.random() * members.length)]; //randomly selecting user from array
+
     msg.say(`${user} was choosen from ${method} members!`);
   }
 }
